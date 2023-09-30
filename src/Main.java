21fe86fc06bd806a884f39498196b8c5bd87da8f
Artem Petrosyan <artem.petrosyan@magenta-technology.com>
2023-09-30 12:02:18 +0200

    variables 4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello Skypro!!!!!!!");
         System.out.println("Hello Skypro!!!!!!!");
         System.out.println("Hello Skypro!!!!!!!");
+        System.out.println("Hello Skypro!!!!!!!");
         var a = 1;
         var b = 2;
         var c = 2;
